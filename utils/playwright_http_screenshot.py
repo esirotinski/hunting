e30cd75fe44941f295f955da2690610180214515
sumbru <h@cker.md>
2024-11-29 14:37:28 +0200

    Skip certificate errors for Playwright browser.

--- utils/playwright_http_screenshot.py
+++ utils/playwright_http_screenshot.py
@@ -2,9 +2,13 @@ from playwright.sync_api import sync_playwright
 
 def save_screenshot(domain):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-        page.goto('https://' + domain)
+        browser = p.chromium.launch(
+            headless=True,
+            args=["--ignore-certificate-errors", "--ignore-certificate-errors-spki-list"]
+        )
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto('https://' + domain, wait_until='networkidle')
         page.screenshot(path="../logs/screenshots/" + domain + ".png", full_page=True)
         browser.close()
 
